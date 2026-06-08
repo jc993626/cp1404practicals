915500390be1cdaaa1593813804b2c2782052457
jc993626 <bmcmurra77@gmail.com>
2026-06-08 11:02:32 +1000

    exception demo complete. added while loop inside try/except. removed except ValueError

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -6,13 +6,17 @@ Answer the following questions:
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
 """
 
+
+
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        denominator = int(input("Wrong value, cannot divide by zero, try again:"))
     fraction = numerator / denominator
     print(fraction)
-except ValueError:
+except ValueError: # occurs when input is a floating point number or a string
     print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
+#except ZeroDivisionError:              Occurs when denominator is 0.
+#    print("Cannot divide by zero!")    Not needed after while loop added.
 print("Finished.")
