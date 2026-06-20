69d4c9be41da66e7b4fab2cfc9e34184bea83044
jc993626 <bmcmurra77@gmail.com>
2026-06-20 13:33:07 +1000

    Added enumerate to total_income

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -14,14 +14,13 @@ def main():
         income = float(input(f"Enter income for month {month}:"))
         incomes.append(income)
 
-    print_report(incomes, number_of_months)
+    print_report(incomes)
 
 
-def print_report(incomes: list[Any], number_of_months: int):
+def print_report(incomes):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
-        income = incomes[month - 1]
+    for month, income in enumerate(incomes, 1):
         total += income
         print(f"Month: {month:2} - Income: ${income:10} - Total: ${total:10}")
         # print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
