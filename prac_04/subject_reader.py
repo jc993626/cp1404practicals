3f8c87ff59ddaa4e27c430c223fec29211992207
jc993626 <bmcmurra77@gmail.com>
2026-06-20 10:11:43 +1000

    added subject_reader changes

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,23 +9,33 @@ FILENAME = "subject_data.txt"
 def main():
     """Program to load and display subject data from file."""
     data = load_data(FILENAME)
-    print(data)
+    print_subject_data(data)
 
 
 def load_data(filename=FILENAME):
-    """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(filename)
-    for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        # Make the number an integer as part of a new, poorly named, list
-        data = [parts[0], parts[1], int(parts[2])]
-        print(data)  # See if that worked
-        print("----------")
-    input_file.close()
+    """Read subject_data from file formatted like: subject,lecturer,number of students."""
+    total_data = []
+    with open(filename) as input_file:
+        for line in input_file:
+            #print(line)  # See what a line looks like
+            #print(repr(line))  # See what a line really looks like
+            line = line.strip()  # Remove the \n
+            parts = line.split(',')  # Separate the subject_data into its parts
+            subject_data = [parts[0], parts[1], int(parts[2])]
+            total_data.append(subject_data)
+            print(subject_data)
+            print("----------")
+
+    return total_data
+# CP1401 is taught by Ada Lovelace and has 192 students (subject, teacher, number_of_students)
+def print_subject_data(data):
+    for subject in data:
+        print(f"{subject[0]} is taught by {subject[1]:12} and has {subject[2]:3} students")
+
+
+
+
+
 
 
 main()
