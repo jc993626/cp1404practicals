361e8b7308ea46088aadba9db7cf6086f95d20fe
jc993626 <bmcmurra77@gmail.com>
2026-06-20 13:51:15 +1000

    Added enumerate for printing

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -2,16 +2,21 @@
 
 def main():
     numbers = get_numbers()
+    for index, number in enumerate(numbers, 1):
+        print(f"Number {index} is {number}")
+
     print(f"The last number is {numbers[-1]}")
-    print(f"The smalles number is {min(numbers)}")
+    print(f"The smallest number is {min(numbers)}")
     print(f"The largest number is {max(numbers)}")
     print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
 
 
 def get_numbers():
     numbers = []
-    for number in range(0, 5):
-        numbers.append(int(input("Please enter a number:")))
+    number = int(input("Please enter a number:"))
+    while number >= 0:
+        numbers.append(number)
+        number = int(input("Please enter a number:"))
 
     print(numbers)
     return numbers
@@ -20,13 +25,13 @@ def get_numbers():
 # for i in range(5):
 #     number = int(input("Number: "))
 #     numbers.append(number)
-# main()
+main()
 
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
-
-username = input("Please enter username:")
-
-if username in usernames:
-    print("Access granted")
-else:
-    print("Access denied")
+# usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+#
+# username = input("Please enter username:")
+#
+# if username in usernames:
+#     print("Access granted")
+# else:
+#     print("Access denied")
