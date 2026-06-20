f133bad85a1ce4ce30c3dfb2036c545d97553da8
jc993626 <bmcmurra77@gmail.com>
2026-06-20 20:58:54 +1000

    Added max_length to print values in dictionary

--- prac_05/walkthrough.py
+++ prac_05/walkthrough.py
@@ -17,3 +17,15 @@ while state_code != "":
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
+
+# for state in CODE_TO_NAME:
+#     print(f"{state:3} is {CODE_TO_NAME[state]:30} ")
+# OR this below,
+# max_length = max(len(name) for name in list(name_to_age.keys()))
+#max_lenth = max(len(state)) for state in list(CODE_TO_NAME.value()):
+max_length = max(len(CODE_TO_NAME[state]) for state in CODE_TO_NAME)
+print(max_length)
+for state in CODE_TO_NAME:
+    print(f"{state:3}   is   {CODE_TO_NAME[state]:{max_length}}")
+
+
