16badfc8b916cc12a5e99e891dfe31d0f34efee3
jc993626 <bmcmurra77@gmail.com>
2026-07-09 22:29:55 +1000

    Add Programming Language class and programm

---
+++ prac_06/languages.py
@@ -0,0 +1,26 @@
+""""""
+
+from programming_language import ProgrammingLanguage
+
+def main():
+
+
+
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic =ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+    # print(python.typing)
+    print(python.is_dynamic())
+    print(ruby.is_dynamic())
+    print(visual_basic.is_dynamic())
+    languages = []
+    languages.append(python)
+    languages.append(ruby)
+    languages.append(visual_basic)
+    print(languages)
+
+
+
+
+main()
---
+++ prac_06/programming_language.py
@@ -0,0 +1,39 @@
+"""Estimated time :80min
+    Actual time : 58min (quicker after doing this weeks seminar)
+"""
+
+
+class ProgrammingLanguage:
+    """Programming Language class."""
+    def __init__(self, programming_name = "", typing = "", reflection = "", year = ""):
+        """Programming Language class constructor."""
+        self.programming_name = programming_name
+        self.typing = typing
+        # self.dynamic = dynamic
+        self.year = year
+        self.reflection = reflection
+
+
+    def is_dynamic(self):
+        """Boolean return for Programming Language typing."""
+        return self.typing == "Dynamic"
+
+
+    def __str__(self):
+        """String return for Class."""
+        return f"{self.programming_name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+
+    def __repr__(self):
+        """Method to print strings inside objects created."""
+        # return str(self)
+        return f"{self.programming_name} {self.typing} {self.reflection} {self.year}"
+#            python = ProgrammingLanguage("Python", "Dynamic", True, 1991)              INPUT
+#           `Python, Dynamic Typing, Reflection=True, First appeared in 1991`           OUTPUT
+
+
+# python = ProgrammingLanguage("Python", "Dynamic", True, 1991)         *** INPUT ***
+# ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+# visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+# print(python)
+
