9914c15137d502da48f34ab1db2109cc33c05ade
jc993626 <bmcmurra77@gmail.com>
2026-07-10 15:58:33 +1000

    Add Guitar class, ready for testing

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -18,12 +18,5 @@ class Guitar:
     def is_vintage(self):
         return self.get_age() >= 50
 
-# `get_age()` - which returns how old the guitar is in years (e.g., in 2022 the L-5 is: 2022 - 1922 = 100).
-#   We could set the "current year" with a CONSTANT or literal, but a better way is to use the system's current year with
-#   something like `datetime.datetime.now().year`
-#
-# - `is_vintage()` - which returns `True` if the guitar is 50 or more years old, `False` otherwise
-#   Hint: try using `get_age()` to simplify the implementation of this method!
-
 
 # `Gibson L-5 CES (1922) : $16,035.40`
