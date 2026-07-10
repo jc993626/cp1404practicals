be19285f8849ee5617fbae130efaf409d5be7edb
jc993626 <bmcmurra77@gmail.com>
2026-07-10 18:46:46 +1000

    Add guitars.py completed

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -2,6 +2,8 @@
 
 import datetime
 
+
+
 class Guitar:
     def __init__(self, name = "", year = 0, cost = 0.0):
         self.name = name
@@ -16,7 +18,16 @@ class Guitar:
         # return f"In {datetime.datetime.now().year} the {self.name} is {age} years old"
 
     def is_vintage(self):
-        return self.get_age() >= 50
+        return "(Vintage)" if self.get_age() >= 50 else ""
+
+
 
 
 # `Gibson L-5 CES (1922) : $16,035.40`
+
+    # def __str__(self):  # __ = dunder methos, double underscore
+    #     if self.is_on_sale:
+    #         on_sale_string = "(on sale)"
+    #     else:
+    #         on_sale_string = ""
+    #     return f"{self.name}, ${self.price:.2f} {on_sale_string}"
---
+++ prac_06/guitars.py
@@ -0,0 +1,25 @@
+"""Guitar data input program. (Name, Year, Cost)."""
+
+from guitar import Guitar
+
+def main():
+    guitars = []
+    guitar_name = input("Enter guitar name: ")
+    while guitar_name != "":
+        guitar_year = int(input("Enter year of guitar manufacture: "))
+        guitar_cost = float(input("Enter guitar cost: "))
+        guitars.append(Guitar(guitar_name, guitar_year, guitar_cost))
+
+        guitar_name = input("Enter next guitar name: ")
+
+    for i, guitar in enumerate(guitars, 1):
+        print(f"Guitar {i}: {guitar.name} {guitar.year}, is worth ${guitar.cost:,.2f} {guitar.is_vintage()}")
+
+main()
+
+
+
+
+
+
+
