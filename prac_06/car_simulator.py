82b159c4fced881c3fded8c274805a51dd6cc8c8
jc993626 <bmcmurra77@gmail.com>
2026-07-15 21:17:15 +1000

    Add completed car_simulator

--- prac_06/car_simulator.py
+++ prac_06/car_simulator.py
@@ -8,30 +8,38 @@ def main():
     """"""
     print("Let's drive!")
     # car = Car(100, "The Beast")
-    car = Car(100, input("Enter car name: "))
+    car = Car(100, input("Enter your car name: "))
+
+    menu_choice = display_menu(car)
+    while menu_choice != "q":
+        if menu_choice == "d":
+            drive_car(car)
+        elif menu_choice == "r":
+            add_fuel(car)
+        else:
+            print("Invalid choice.")
+        menu_choice = display_menu(car)
+    print(f"Good bye {car.name}'s driver.")
+
+
+def display_menu(car):
     print(car)
     print("Menu: ")
     print("d) drive")
     print("r) refuel")
     print("q) quit")
     menu_choice = input("Enter your choice: ")
-
-    while menu_choice != "q":
-        if menu_choice == "d":
-            print("How many km do you wish to drive? ")
-    try:
-
-
+    return menu_choice
 
 
 def add_fuel(car: Car):
     add_fuel_input = False
     while not add_fuel_input:
         try:
-            add_fuel_amount = int(input("How many km do you wish to drive? "))
+            add_fuel_amount = int(input("How many units of fuel do you want to add to the car? "))
             if add_fuel_amount < 0:
                 print("Distance must be >= 0.")
-                add_fuel_amount = int(input("How many km do you wish to drive? "))
+                add_fuel_amount = int(input("How many units of fuel do you want to add to the car? "))
             else:
                 add_fuel_input = True
         except ValueError:
@@ -53,24 +61,9 @@ def drive_car(car: Car):
             print("Must be whole number!")
     print(car.drive(drive_distance))
 
-
-
-
-
-
 main()
 
 
-
-
-
-
-    # car = Car(200, "The Beast")
-    # print(car)
-    # car.drive(50)
-    # print(car)
-    # car.add_fuel(50)
-    # print(car)
 
 
 
