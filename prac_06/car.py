ecc78a7a77243c20ed1b6c608125a6fe576b4c6e
jc993626 <bmcmurra77@gmail.com>
2026-07-15 20:41:04 +1000

    Add drive and refuel functions to car_simulator

--- prac_06/car.py
+++ prac_06/car.py
@@ -19,19 +19,20 @@ class Car:
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
         self.fuel += amount
+        return f"Added {amount} units of fuel."
+
 
     def drive(self, distance):
         """Drive the car a given distance.
-
         Drive given distance if car has enough fuel
         or drive until fuel runs out return the distance actually driven.
         """
-
         if distance > self.fuel:
             distance = self.fuel
             self.fuel = 0
-            distance_string = f""
+            distance_string = f"{distance} km and ran out of fuel."
         else:
             self.fuel -= distance
+            distance_string = f"The car drove {distance} km."
         self._odometer += distance
-        return f"The car drove {distance_string} km."
+        return distance_string
--- prac_06/car_simulator.py
+++ prac_06/car_simulator.py
@@ -1,27 +1,45 @@
 """Car simulator using Car class"""
 
 from car import Car
+from prac_06.car import Car
+
 
 def main():
     """"""
-    # print("Let's drive!")
-    car = Car(100, "The Beast")
-    # car = Car(100, input("Enter car name: "))
-    # print(car)
-    # print("Menu: ")
-    # print("d) drive")
-    # print("r) refuel")
-    # print("q) quit")
-    # menu_choice = input("Enter your choice: ")
+    print("Let's drive!")
+    # car = Car(100, "The Beast")
+    car = Car(100, input("Enter car name: "))
+    print(car)
+    print("Menu: ")
+    print("d) drive")
+    print("r) refuel")
+    print("q) quit")
+    menu_choice = input("Enter your choice: ")
 
-    # while menu_choice != "q":
-    #     if menu_choice == "d":
-    #         print("How many km do you wish to drive? ")
-    # try:
-
-    # drive_distance = 50
+    while menu_choice != "q":
+        if menu_choice == "d":
+            print("How many km do you wish to drive? ")
+    try:
 
 
+
+
+def add_fuel(car: Car):
+    add_fuel_input = False
+    while not add_fuel_input:
+        try:
+            add_fuel_amount = int(input("How many km do you wish to drive? "))
+            if add_fuel_amount < 0:
+                print("Distance must be >= 0.")
+                add_fuel_amount = int(input("How many km do you wish to drive? "))
+            else:
+                add_fuel_input = True
+        except ValueError:
+            print("Must be whole number!")
+    print(car.add_fuel(add_fuel_amount))
+
+
+def drive_car(car: Car):
     drive_input = False
     while not drive_input:
         try:
@@ -34,7 +52,6 @@ def main():
         except ValueError:
             print("Must be whole number!")
     print(car.drive(drive_distance))
-
 
 
 
