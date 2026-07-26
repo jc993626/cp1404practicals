46871795b0a5c5211e08f8a237276e710e94e9fd
jc993626 <bmcmurra77@gmail.com>
2026-07-26 21:14:21 +1000

    Add walkthrough example box_layout_demo.py and box_layout.kv

--- prac_08/kivy_demos/box_layout_demo.py
+++ prac_08/kivy_demos/box_layout_demo.py
@@ -8,6 +8,13 @@ class BoxLayoutDemo(App):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
+    def handle_greet(self):
+        print("Greet")
+        self.root.ids.output_label.text = "Hello"
+        self.root.ids.input_name.text = f"Hello {self.root.ids.input_name.text}"
+    def handle_clear(self):
+        self.root.ids.output_label.text = ''
+        self.root.ids.input_name.text = ''
 
 
 BoxLayoutDemo().run()
